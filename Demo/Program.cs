ee9af11d5c9e6ea50dd756d707138ea913979803
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-02-01 09:38:24 +0530

    UC1 test

---
+++ Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1.MyMethodUC1();
+            UC1 uC1 = new UC1();
+
+            uC1.MyMethodUC2();
+
+        }
+    }
+}
---
+++ Demo/UC1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class UC1
+    {
+        public static void MyMethodUC1()
+        {
+            Console.WriteLine("hello world uc1");
+        }
+        public void MyMethodUC2() 
+        {
+            Console.WriteLine("hello world uc2 ");
+        }
+    }
+}
